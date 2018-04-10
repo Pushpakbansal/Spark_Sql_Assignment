59c903c80f1317fa7528b0353ef8fb44fbf9621d
Pushpakbansal <36412945+Pushpakbansal@users.noreply.github.com>
2018-04-10 11:59:50 +0530

    Add files via upload

--- SparkAssign.scala
+++ SparkAssign.scala
@@ -27,7 +27,6 @@ object SparkAssign {
     return orderItem
   }  
   
-  // To be done
   case class Orders(Order_id:Int, Order_dt:String , Order_customer_id:Int, order_status:String)
   
   def mapper3(line:String): Orders = {
@@ -61,11 +60,10 @@ object SparkAssign {
       .getOrCreate()
     
     // This is for the local machine, so in the E drive.
-    //val products = spark.sparkContext.textFile("E:/Spark_Scala/SparkScala_Documents/Products.txt")
-    val products = spark.sparkContext.textFile("https://github.com/Pushpakbansal/Spark_Sql_Assignment/blob/master/Products.txt")
-    val customers= spark.sparkContext.textFile("https://github.com/Pushpakbansal/Spark_Sql_Assignment/blob/master/Customers.txt")
-    val orders= spark.sparkContext.textFile("https://github.com/Pushpakbansal/Spark_Sql_Assignment/blob/master/Orders.txt")
-    val ordersitem= spark.sparkContext.textFile("https://github.com/Pushpakbansal/Spark_Sql_Assignment/blob/master/Order_items.txt")
+    val products = spark.sparkContext.textFile("E:/Spark_Scala/SparkScala_Documents/Products.txt")
+    val customers= spark.sparkContext.textFile("E:/Spark_Scala/SparkScala_Documents/Customers.txt")
+    val orders= spark.sparkContext.textFile("E:/Spark_Scala/SparkScala_Documents/Orders.txt")
+    val ordersitem= spark.sparkContext.textFile("E:/Spark_Scala/SparkScala_Documents/Order_items.txt")
     
     val products_rdd = products.map(mapper1)
     val ordersitem_rdd = ordersitem.map(mapper2)
@@ -97,10 +95,7 @@ object SparkAssign {
     // Total sales for each month
     val sum_sale_month = spark.sql("SELECT mth, sum(sales) from (SELECT month(to_date(o.Order_dt)) as mth, Order_item_subtotal as sales FROM Orders o JOIN Order_Item o_i on o_i.Order_item_order_id = o.Order_id) tbl_1 group by mth")
     
-    /**
-     * Average sales for each date
-     * 1. In the Inner query get the  
-     */
+    // Average sales for each date
     val avg_sale_date = spark.sql("SELECT Orders.Order_dt,avg(Order_item_subtotal) FROM Orders JOIN Order_Item on Order_Item.Order_item_order_id = Orders.Order_id group by Orders.Order_dt")
     
     // Average sales for month
@@ -110,8 +105,9 @@ object SparkAssign {
     val top_products = spark.sql("Select Product_name,Product_id from (select Order_item_product_id from (select Order_item_product_id, dense_rank() over (order by sales) as rnk from (select Order_item_product_id, sum(Order_item_subtotal) as sales from Order_Item group by Order_item_product_id) tbl_1) tbl_2 where rnk <= 10) tbl_3 JOIN Products on Order_item_product_id = Product_id") 
     
     // Top 3 purchased customers for each month
-    val top_customer = spark.sql("select mth, c_id, rnk, cnt from (select mth, c_id, dense_rank() over(partition by mth order by cnt desc) as rnk, cnt from (Select month(to_date(Order_dt)) as mth, Customer_id as c_id, count(Order_customer_id) as cnt from Orders o JOIN Customers c on o.Order_customer_id = c.Customer_id group by month(to_date(Order_dt)),Customer_id ) tbl_1) tbl_2 where rnk <=3")
-    //top_customer.printSchema()
+     val top_customer = spark.sql("select mth, c_id, rnk, cnt from (select mth, c_id, dense_rank() over(partition by mth order by cnt desc) as rnk, cnt from (Select month(to_date(Order_dt)) as mth, Customer_id as c_id, count(Order_customer_id) as cnt from Orders o JOIN Customers c on o.Order_customer_id = c.Customer_id group by month(to_date(Order_dt)),Customer_id ) tbl_1) tbl_2 where rnk <=3")
+    
+	//top_customer.printSchema()
     
     // Top produce by sale for each month
     val top_prod_by_sale = spark.sql("select mth,Product_name from( select mth, p_id from (select mth, p_id, dense_rank() over (partition by mth, p_id order by sales desc) as rnk from (select mth, p_id, sum(qty) sales from (select Order_item_product_id as p_id, Order_item_quantity as qty , month(to_date(Order_dt)) as mth From Orders od JOIN Order_Item oi on od.Order_id = oi.Order_item_order_id) tbl_1 group by mth,p_id) tbl_2) tbl_3 where rnk = 1) tbl_4 JOIN Products on tbl_4.p_id = Products.Product_id")
@@ -122,9 +118,7 @@ object SparkAssign {
     // Most popular product category 
     val popular_prod_ctg = spark.sql("select p.Product_category_id from ( select p_id, dense_rank() over (order by sales) as rnk from (select Order_item_product_id as p_id, sum(Order_item_quantity) as sales from Order_Item group by Order_item_product_id) tbl_1 ) tbl_2 JOIN Products p where p_id = p.Product_id and rnk =1")
     
-    /*
-     * Try any of the above and check the results below
-     * */
+	// To see the results get the collect method called.
     val results = top_products.collect()
     
     results.foreach(println)
